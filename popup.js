06eff210b0a484b531e0ae7d811b978522c6be02
akmr328 <akm328@gmail.com>
2021-03-11 12:06:34 -0500

    datetime field input viewable not functional, removed all react/redux back to simple

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
 
 var currentURL = document.getElementById('currentTab');
 
+
+//email toggle
+const emailButton = document.getElementById("email")
+
+
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, 
 function(tabs){
 	getCurrentURL(tabs[0].url);
@@ -25,3 +30,22 @@ chrome.bookmarks.create({
   'title': 'Extensions doc',
   'url': 'http://code.google.com/chrome/extensions',
 });
+
+//create timer/countdown
+let selectedTime = document.querySelector(".selectedTime")
+console.log(selectedTime)
+let sec = setInterval(function(){
+  const now = new Date().getTime();
+  const distance = selectedTime - now;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  document.getElementById("countdown").innerHTML = days + "d" + hours + "h" + minutes + "m" + seconds + "s"
+   if (distance < 0) {
+     clearInterval(sec)
+     document.getElementById("countdown").innerHTML = "Times up!"
+   }
+}, 1000)
+
